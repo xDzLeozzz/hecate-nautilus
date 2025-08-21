926f8c7c9929035f7ef1ddc0566df455a71e36e0
Leonardo Souza Temponi <leonardotemponi360@gmail.com>
2025-08-21 10:14:35 -0300

    Feat: Adiciona entrypoint para corrigir permissões em tempo de execução

---
+++ unbound/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Hecate-Nautilus Entrypoint
+
+# Saia imediatamente se um comando falhar
+set -e
+
+# Valida o arquivo de configuração antes de prosseguir
+echo "Validating Unbound configuration..."
+unbound-checkconf /etc/unbound/unbound.conf
+
+# Corrige as permissões da pasta de configuração para o usuário 'unbound'
+echo "Setting ownership for /etc/unbound..."
+chown -R unbound:unbound /etc/unbound
+
+# Entrega o controle para o processo principal do Unbound,
+# rodando como o usuário 'unbound'
+exec gosu unbound unbound -d -c /etc/unbound/unbound.conf
